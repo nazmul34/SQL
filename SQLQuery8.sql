2a16a5d241339a6db2f0507adedbde84a96f6f23
Nazmul Islam <nazmul.islam.6978@gmail.com>
2021-08-05 09:57:58 +0600

    derived tables and cte

---
+++ SQLQuery8.sql
@@ -0,0 +1,116 @@
+-- In this sql we will learn about, Derived tables and common table expressions (CTE's). We will also explore the differences between Views, Table Variable, Local and Global Temp Tables, 
+-- Derived tables and common table expressions.
+
+-- Let's create the required Employee and Department tables, that we will be using for this demo.
+
+CREATE TABLE tblEmployee
+(
+  Id int Primary Key,
+  Name nvarchar(30),
+  Gender nvarchar(10),
+  DepartmentId int
+)
+
+CREATE TABLE tblDepartment
+(
+ DeptId int Primary Key,
+ DeptName nvarchar(20)
+)
+
+--Insert data into tblDepartment table
+Insert into tblDepartment values (1,'IT')
+Insert into tblDepartment values (2,'Payroll')
+Insert into tblDepartment values (3,'HR')
+Insert into tblDepartment values (4,'Admin')
+
+--Insert data into tblEmployee table
+Insert into tblEmployee values (1,'John', 'Male', 3)
+Insert into tblEmployee values (2,'Mike', 'Male', 2)
+Insert into tblEmployee values (3,'Pam', 'Female', 1)
+Insert into tblEmployee values (4,'Todd', 'Male', 4)
+Insert into tblEmployee values (5,'Sara', 'Female', 1)
+Insert into tblEmployee values (6,'Ben', 'Male', 3)
+
+
+-- Now, we want to write a query which would return the following output. The query should return, the Department Name and Total Number of employees, with in the department.
+-- The departments with greatar than or equal to 2 employee should only be returned. Obviously, there are severl ways to do this
+
+--Script to create the View
+Create view vWEmployeeCount
+as
+Select DeptName, DepartmentId, COUNT(*) as TotalEmployees
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+group by DeptName, DepartmentId
+
+--Query using the view:
+Select DeptName, TotalEmployees 
+from vWEmployeeCount
+where  TotalEmployees >= 2
+
+
+-- Now, let's see, how to achieve the same using, temporary tables.
+
+
+-- local temporary tables
+-- replace #TempEmployeeCount with ##TempEmployeeCount to get the same thing for global temp veriables
+Select DeptName, DepartmentId, COUNT(*) as TotalEmployees
+into #TempEmployeeCount
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+group by DeptName, DepartmentId
+
+Select DeptName, TotalEmployees
+From #TempEmployeeCount
+where TotalEmployees >= 2
+
+Drop Table #TempEmployeeCount
+
+
+--Using Table Variable:
+Declare @tblEmployeeCount table
+(DeptName nvarchar(20),DepartmentId int, TotalEmployees int)
+
+Insert @tblEmployeeCount
+Select DeptName, DepartmentId, COUNT(*) as TotalEmployees
+from tblEmployee
+join tblDepartment
+on tblEmployee.DepartmentId = tblDepartment.DeptId
+group by DeptName, DepartmentId
+
+Select DeptName, TotalEmployees
+From @tblEmployeeCount
+where  TotalEmployees >= 2
+
+
+--Using Derived Tables:
+Select DeptName, TotalEmployees
+from 
+ (
+  Select DeptName, DepartmentId, COUNT(*) as TotalEmployees
+  from tblEmployee
+  join tblDepartment
+  on tblEmployee.DepartmentId = tblDepartment.DeptId
+  group by DeptName, DepartmentId
+ ) 
+as EmployeeCount
+where TotalEmployees >= 2
+
+
+
+--Using CTE:
+With EmployeeCount(DeptName, DepartmentId, TotalEmployees)
+as
+(
+ Select DeptName, DepartmentId, COUNT(*) as TotalEmployees
+ from tblEmployee
+ join tblDepartment
+ on tblEmployee.DepartmentId = tblDepartment.DeptId
+ group by DeptName, DepartmentId
+)
+
+Select DeptName, TotalEmployees
+from EmployeeCount
+where TotalEmployees >= 2
